6db044ff31dab2a81708b6cc458477c8e46ea382
Ekwarin Yeoh <ekwarin@dek-d.com>
2023-03-09 14:47:09 +0700

    feat: add last page of viewpager swipe detection

--- readium/navigator/src/main/java/org/readium/r2/navigator/pager/R2ViewPager.kt
+++ readium/navigator/src/main/java/org/readium/r2/navigator/pager/R2ViewPager.kt
@@ -17,10 +17,14 @@ import org.readium.r2.navigator.BuildConfig.DEBUG
 import org.readium.r2.shared.publication.Publication
 import timber.log.Timber
 
+
 class R2ViewPager : ViewPager {
 
 
     lateinit var type: Publication.TYPE
+
+    var mStartDragX = 0f
+    var mOnSwipeOutListener: OnSwipeOutListener? = null
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
@@ -29,16 +33,40 @@ class R2ViewPager : ViewPager {
         super.setCurrentItem(item, false)
     }
 
+    fun setOnSwipeOutListener(listener: OnSwipeOutListener) {
+        mOnSwipeOutListener = listener
+    }
+
+    private fun onSwipeOutAtStart() {
+        if (mOnSwipeOutListener != null) {
+            mOnSwipeOutListener!!.onSwipeOutAtStart()
+        }
+    }
+
+    private fun onSwipeOutAtEnd() {
+        if (mOnSwipeOutListener != null) {
+            mOnSwipeOutListener!!.onSwipeOutAtEnd()
+        }
+    }
+
     override fun onTouchEvent(ev: MotionEvent): Boolean {
         if (DEBUG) Timber.d("ev.action ${ev.action}")
         if (type == Publication.TYPE.EPUB) {
-            when (ev.action and MotionEvent.ACTION_MASK) {
-                MotionEvent.ACTION_DOWN -> {
-                    // prevent swipe from view pager directly
-                    if (DEBUG) Timber.d("ACTION_DOWN")
-                    return false
+            if (currentItem == adapter!!.count - 1) {
+                val x: Float = ev.x
+                when (ev.action and MotionEvent.ACTION_MASK) {
+                    MotionEvent.ACTION_DOWN -> mStartDragX = x
+                    MotionEvent.ACTION_MOVE -> {}
+                    MotionEvent.ACTION_UP -> if (x < mStartDragX) {
+                        onSwipeOutAtEnd()
+                    } else {
+                        mStartDragX = 0f
+                    }
                 }
+            } else {
+                mStartDragX = 0f
             }
+            return super.onTouchEvent(ev)
         }
 
         return try {
@@ -57,8 +85,9 @@ class R2ViewPager : ViewPager {
         if (type == Publication.TYPE.EPUB) {
             when (ev.action and MotionEvent.ACTION_MASK) {
                 MotionEvent.ACTION_DOWN -> {
+                    mStartDragX = ev.x
                     // prevent swipe from view pager directly
-                    return false
+                    return super.onInterceptTouchEvent(ev)
                 }
             }
         }
@@ -72,6 +101,11 @@ class R2ViewPager : ViewPager {
             Timber.e(ex)
             false
         }
+    }
+
+    interface OnSwipeOutListener {
+        fun onSwipeOutAtStart()
+        fun onSwipeOutAtEnd()
     }
 
 }
